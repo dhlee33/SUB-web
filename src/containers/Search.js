4f1182f7a3b71d3fd4b7cb584142bbd31fd689d0
ydh1304 <ydh1304@naver.com>
2018-05-28 15:57:12 +0900

    coding convention

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -11,14 +11,14 @@ class Search extends Component <Props> {
   constructor(props) {
     super(props);
     this.state = {
-      query: this.props.query || '',
+      query: props.query || '',
     };
     this.onInputChange = this.onInputChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   onInputChange(e) {
-    this.setState({query: e.target.value});
+    this.setState({ query: e.target.value });
   }
 
   onSubmit(e) {
@@ -29,13 +29,13 @@ class Search extends Component <Props> {
 
   render() {
     return (
-      <Row style={{justifyContent: 'flex-end'}}>
+      <Row style={{ justifyContent: 'flex-end' }}>
         <Col sm={5}>
           <Form onSubmit={e => this.onSubmit(e)}>
-          <InputGroup>
-            <Input value={this.state.query} onChange={e => this.onInputChange(e)} />
-            <Button type="submit">검색</Button>
-          </InputGroup>
+            <InputGroup>
+              <Input value={this.state.query} onChange={e => this.onInputChange(e)} />
+              <Button type="submit">검색</Button>
+            </InputGroup>
           </Form>
         </Col>
       </Row>
